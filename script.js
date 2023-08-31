fb5a0b4cb5ec2c621d03b33a30c6899103e7f91a
Stanley-Rodrigues <stanleyrodriguessilva@gmail.com>
2023-08-31 15:13:24 -0300

    up project

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function generateNumber() {
+
+    const min = Math.ceil(document.querySelector(".min").value)
+    const max = Math.floor(document.querySelector(".max").value)
+
+    const result = Math.floor(Math.random() * (max - min + 1) + min)
+
+    console.log(result) 
+
+    alert(result)
+}
+
+
